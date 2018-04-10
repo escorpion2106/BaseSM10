6fbe19228685a4502b12182e3a3793c4c0af1896
escorpion2106 <deluchidiego@gmail.com>
2018-04-10 05:36:31 +0200

    Navegacion

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"./Formatter"
-], function(Controller, Formatter) {
+	"./BaseController"
+], function(BaseController,Controller) {
 	"use strict";
 
 	return Controller.extend("Zsm10.controller.Main", {
+
+	click: function(event) {
+		var item = event.getSource().getSelectedItem();
+		var valor = item.getBindingContext().getProperty("RecordId");
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		oRouter.navTo("Detalles");
+	}
 
 	});
 });
