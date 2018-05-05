26d2fc9827cdb3a18ce08a76c7ecaa6ca5257a1e
escorpion2106 <deluchidiego@gmail.com>
2018-05-05 16:51:31 -0300

    Comienzo BInding Detalles

--- webapp/controller/Detalles.controller.js
+++ webapp/controller/Detalles.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 	"./BaseController"
-], function(BaseController, Controller) {
+], function(BaseController, JSONModel, Controller) {
 	"use strict";
 
 
@@ -12,9 +13,20 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf Zsm10.view.view.Detalles
 		 */
-		//	onInit: function() {
-		//
-		//	},
+			onInit: function() {
+								// Model used to manipulate control states. The chosen values make sure,
+				// detail page is busy indication immediately so there is no break in
+				// between the busy indication for loading the view's meta data
+				var oViewModel = new JSONModel({
+					busy : false,
+					delay : 0,
+					lineItemListTitle : "detailLineItemTableHeading"
+				});
+				
+				this.getRouter().getRoute("Detalles").attachPatternMatched(this._onObjectMatched, this);
+				
+				this.setModel(oViewModel, "detailView");
+			},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -41,6 +53,80 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
+
+			/**
+			 * Binds the view to the object path and expands the aggregated line items.
+			 * @function
+			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+			 * @private
+			 */
+			_onObjectMatched : function (oEvent) {
+				var sObjectId =  oEvent.getParameter("arguments").objectId;
+				this.getModel().metadataLoaded().then( function() {
+					var sObjectPath = this.getModel().createKey("HorasSet", {
+						RecordId :  sObjectId
+					});
+					this._bindView("/" + sObjectPath);
+				}.bind(this));
+			},
+
+			/**
+			 * Binds the view to the object path. Makes sure that detail view displays
+			 * a busy indicator while data for the corresponding element binding is loaded.
+			 * @function
+			 * @param {string} sObjectPath path to the object to be bound to the view.
+			 * @private
+			 */
+			_bindView : function (sObjectPath) {
+				// Set busy indicator during view binding
+				var oViewModel = this.getModel("detailView");
+
+				// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+				oViewModel.setProperty("/busy", false);
+
+				this.getView().bindElement({
+					path : sObjectPath,
+					events: {
+						change : this._onBindingChange.bind(this),
+						dataRequested : function () {
+							oViewModel.setProperty("/busy", true);
+						},
+						dataReceived: function () {
+							oViewModel.setProperty("/busy", false);
+						}
+					}
+				});
+			},
+			
+			_onBindingChange : function () {
+				var oView = this.getView(),
+					oElementBinding = oView.getElementBinding();
+
+				// No data for the binding
+				if (!oElementBinding.getBoundContext()) {
+					this.getRouter().getTargets().display("Detalles");
+					// if object could not be found, the selection in the master list
+					// does not make sense anymore.
+				//	this.getOwnerComponent().oListSelector.clearMasterListSelection();
+					return;
+				}
+
+				var sPath = oElementBinding.getPath(),
+					oResourceBundle = this.getResourceBundle(),
+					oObject = oView.getModel().getObject(sPath),
+					sObjectId = oObject.Consultor,
+					sObjectName = oObject.Consultor,
+					oViewModel = this.getModel("detailView");
+
+				this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+
+				oViewModel.setProperty("/saveAsTileTitle",oResourceBundle.getText("shareSaveTileAppTitle", [sObjectName]));
+				oViewModel.setProperty("/shareOnJamTitle", sObjectName);
+				oViewModel.setProperty("/shareSendEmailSubject",
+					oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+				oViewModel.setProperty("/shareSendEmailMessage",
+					oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+			}			
 
 	});
 
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,52 +1,56 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/Filter",
-
 	"./BaseController"
-], function(BaseController,Filter,Controller) {
+], function(BaseController, Filter, Controller) {
 	"use strict";
 
 	return Controller.extend("Zsm10.controller.Main", {
 
-onInit: function () {
-	var filters = [];
-	var oFilter = new Filter("ZZUSUARIO", sap.ui.model.FilterOperator.Contains, "ROGI");
-				filters.push(oFilter);
+		onInit: function() {
 
-    var list = this.getView().byId("table_imputacion");
-    var binding = list.getBinding("items");
-    binding.filter(filters);
-	},
+		},
 
+		click: function(event) {
 
-	click: function(event) {
+			var item = event.getSource().getSelectedItem();
+			var valor = item.getBindingContext().getProperty("RecordId");
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("Detalles",{algo: valor});
+		},
+
+		onSelectChange: function(event) {
+	/*		var filters = [];
+			var oFilter = new Filter("Zzusuario", sap.ui.model.FilterOperator.Contains, event.getSource().getValue());
+			filters.push(oFilter);
+
+			var list = this.getView().byId("table_imputacion");
+			var binding = list.getBinding("items");
+			binding.filter(filters);*/
+
+		},
 		
-		var item = event.getSource().getSelectedItem();
-		var valor = item.getBindingContext().getProperty("RecordId");
-		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		oRouter.navTo("Detalles");
-	},
-
-	onSelectChange: function(event){
-	// 	var oFilter = null;
-	// 	oFilter = new sap.ui.model.Filter([this._oGlobalFilter, this._oPriceFilter], true);
-	// 	this.getView().byId("table_imputacion").getBinding("rows").filter(oFilter, "Application");
-	// }
-	    var filters = [];
-    // var query = event.getParameter("query");
-    // if (query && query.length > 0) {
-    //     var nameFilter = new sap.ui.model.Filter("name", sap.ui.model.FilterOperator.Contains, query);
-
-    //     filters.push(nameFilter);
-    // }
-
-var oFilter = new Filter("ZZUSUARIO", sap.ui.model.FilterOperator.Contains, "ROGI");
+		onSearch: function(event) {
+			var filters = [];
+			var input = this.getView().byId("i1");
+			if ( input.getValue() ){
+				var oFilter = new Filter("ZzobjectId", sap.ui.model.FilterOperator.Contains, input.getValue());
 				filters.push(oFilter);
-
-    var list = this.getView().byId("table_imputacion");
-    var binding = list.getBinding("items");
-    binding.filter(filters);
-	}
-	
+			}
+			input = this.getView().byId("i2");
+			if ( input.getValue() ){
+				oFilter = new Filter("ZzproySra", sap.ui.model.FilterOperator.Contains, input.getValue());
+				filters.push(oFilter);
+			}
+			input = this.getView().byId("i3");
+			if ( input.getValue() ){
+				oFilter = new Filter("Zzusuario", sap.ui.model.FilterOperator.Contains, input.getValue());
+				filters.push(oFilter);
+			}
+			
+			var list = this.getView().byId("table_imputacion");
+			var binding = list.getBinding("items");
+			binding.filter(filters);
+		}
 	});
 });
