f060b55abf0421f9d61cc5d77be0465306e53900
escorpion2106 <deluchidiego@gmail.com>
2018-05-07 01:06:46 +0200

    puto git

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -14,8 +14,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"], fun
 			} else {
 				this.getRouter().navTo("appHome", {}, true /*no history*/ );
 			}
-<<<<<<< Upstream, based on 26d2fc9827cdb3a18ce08a76c7ecaa6ca5257a1e
-<<<<<<< Upstream, based on 26d2fc9827cdb3a18ce08a76c7ecaa6ca5257a1e
 
 		},
 
@@ -40,34 +38,5 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"], fun
 			setModel : function (oModel, sName) {
 				return this.getView().setModel(oModel, sName);
 			}		
-=======
-		}
->>>>>>> 88d776e test3
-=======
-
-		},
-
-		/**
-		 * Convenience method for getting the view model by name in every controller of the application.
-		 * @public
-		 * @param {string} sName the model name
-		 * @returns {sap.ui.model.Model} the model instance
-		 */
-		getModel: function(sName) {
-			return this.getView().getModel(sName);
-		},
-		
-		
-			/**
-			 * Convenience method for setting the view model in every controller of the application.
-			 * @public
-			 * @param {sap.ui.model.Model} oModel the model instance
-			 * @param {string} sName the model name
-			 * @returns {sap.ui.mvc.View} the view instance
-			 */
-			setModel : function (oModel, sName) {
-				return this.getView().setModel(oModel, sName);
-			}		
->>>>>>> 68206c9 asd
 	});
 });
